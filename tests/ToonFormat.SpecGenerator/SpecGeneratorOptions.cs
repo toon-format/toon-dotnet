a5aad2f0e502de465511119d5f5747fb34beae95
Daniel Destouche <daniel.destouche@gmail.com>
2025-11-13 23:13:49 -0500

    feat: Spec generator utility for aligning tests with spec

---
+++ tests/ToonFormat.SpecGenerator/SpecGeneratorOptions.cs
@@ -0,0 +1,36 @@
+using CommandLine;
+
+namespace ToonFormat.SpecGenerator;
+
+public class SpecGeneratorOptions
+{
+    /// <summary>
+    /// Thhe git repo of the toon spec
+    /// </summary>
+    [Option('u', "url", Required = true, HelpText = "The git repo of the toon spec")]
+    public required string SpecRepoUrl { get; set; }
+
+    /// <summary>
+    /// The relative path of the output directory
+    /// </summary>
+    [Option('o', "output", Required = true, HelpText = "The relative path of the output directory")]
+    public required string OutputPath { get; set; }
+
+    /// <summary>
+    /// The relative path to a .specignore file, to explicitly ignore generating tests based on a test name
+    /// </summary>
+    [Option('i', "ignore", Required = false, HelpText = "The relative path to a .specignore file, to explicitly ignore generating tests based on a test name")]
+    public string? IgnorePath { get; set; }
+
+    /// <summary>
+    /// The branch version or tag name to reference
+    /// </summary>
+    [Option('b', "branch", Required = false, HelpText = "The branch version or tag name to reference")]
+    public string? Branch { get; set; }
+
+    internal string AbsoluteOutputPath => Path.GetFullPath(
+                        Path.Combine(Environment.GetEnvironmentVariable("PWD") ?? Environment.CurrentDirectory, OutputPath));
+
+    internal string AbsoluteSpecIgnorePath => Path.GetFullPath(
+                        Path.Combine(Environment.GetEnvironmentVariable("PWD") ?? Environment.CurrentDirectory, IgnorePath ?? ""));
+}
